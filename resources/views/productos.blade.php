55dd15e8c37f4a4e751645dc7437fdd6294d6e87
tatenalan <taten.usa@gmail.com>
2020-01-29 19:46:38 -0300

    probando imagenes productos

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -3,8 +3,123 @@
 IL Nato Home
 @endsection
 @section('css')
-productos
+Productos
 @endsection('css')
 @section('main')
+
+  <div class="container">
+    <section class="productos">
+        <div class="row">
+          <div class="padding  col-6 col-md-4 col-lg-3">
+            <div class="producto">
+                <a href="/"><img src="/img/producto5a.jpg" style="" class="img-productos"></a>
+                <h3>Zapatilla</h3>
+                <p class="descripcion">Yeezy</p>
+              <a class="ordenar" href="/">Ordenar!  <ion-icon name="cart"></ion-icon></a>
+            </div>
+          </div>
+
+          <div class="padding  col-6 col-md-4 col-lg-3">
+            <div class="producto">
+                <a href="/"><img src="/img/producto2a.jpg" style="" class="img-productos"></a>
+              <h3>Zapatilla</h3>
+              <p class="descripcion">Yeezy</p>
+              <a class="ordenar" href="/">Ordenar!  <ion-icon name="cart"></ion-icon></a>
+            </div>
+          </div>
+
+            <div class="padding  col-6 col-md-4 col-lg-3">
+              <div class="producto">
+                <a href="/"><img src="/img/producto3a.jpg" style="" class="img-productos"></a>
+                <h3>Zapatilla</h3>
+                <p class="descripcion">Yeezy</p>
+              <a class="ordenar" href="/">Ordenar!  <ion-icon name="cart"></ion-icon></a>
+            </div>
+          </div>
+
+          <div class="padding  col-6 col-md-4 col-lg-3">
+            <div class="producto">
+              <a href="/"><img src="/img/producto3a.jpg" style="" class="img-productos"></a>
+              <h3>Zapatilla</h3>
+              <p class="descripcion">Yeezy</p>
+            <a class="ordenar" href="/">Ordenar!  <ion-icon name="cart"></ion-icon></a>
+            </div>
+          </div>
+
+          <div class="padding  col-6 col-md-4 col-lg-3">
+            <div class="producto">
+                <a href="/"><img src="/img/producto5a.jpg" style="" class="img-productos"></a>
+                <h3>Zapatilla</h3>
+                <p class="descripcion">Yeezy</p>
+              <a class="ordenar" href="/">Ordenar!  <ion-icon name="cart"></ion-icon></a>
+            </div>
+          </div>
+
+          <div class="padding  col-6 col-md-4 col-lg-3">
+            <div class="producto">
+                <a href="/"><img src="/img/producto5a.jpg" style="" class="img-productos"></a>
+                <h3>Zapatilla</h3>
+                <p class="descripcion">Yeezy</p>
+              <a class="ordenar" href="/">Ordenar!  <ion-icon name="cart"></ion-icon></a>
+            </div>
+          </div>
+
+          <div class="padding  col-6 col-md-4 col-lg-3">
+            <div class="producto">
+                <a href="/"><img src="/img/producto5a.jpg" style="" class="img-productos"></a>
+                <h3>Zapatilla</h3>
+                <p class="descripcion">Yeezy</p>
+              <a class="ordenar" href="/">Ordenar!  <ion-icon name="cart"></ion-icon></a>
+            </div>
+          </div>
+
+          <div class="padding  col-6 col-md-4 col-lg-3">
+            <div class="producto">
+                <a href="/"><img src="/img/producto5a.jpg" style="" class="img-productos"></a>
+                <h3>Zapatilla</h3>
+                <p class="descripcion">Yeezy</p>
+              <a class="ordenar" href="/">Ordenar!  <ion-icon name="cart"></ion-icon></a>
+            </div>
+          </div>
+
+          <div class="padding  col-6 col-md-4 col-lg-3">
+            <div class="producto">
+                <a href="/"><img src="/img/1.jpg" style="" class="img-productos"></a>
+                <h3>Zapatilla</h3>
+                <p class="descripcion">Yeezy</p>
+              <a class="ordenar" href="/">Ordenar!  <ion-icon name="cart"></ion-icon></a>
+            </div>
+          </div>
+
+          <div class="padding  col-6 col-md-4 col-lg-3">
+            <div class="producto">
+                <a href="/"><img src="/img/a1.jpg" style="" class="img-productos"></a>
+                <h3>Zapatilla</h3>
+                <p class="descripcion">Yeezy</p>
+              <a class="ordenar" href="/">Ordenar!  <ion-icon name="cart"></ion-icon></a>
+            </div>
+          </div>
+
+          <div class="padding  col-6 col-md-4 col-lg-3">
+            <div class="producto">
+                <a href="/"><img src="/img/1.jpg" style="" class="img-productos"></a>
+                <h3>Zapatilla</h3>
+                <p class="descripcion">Yeezy</p>
+              <a class="ordenar" href="/">Ordenar!  <ion-icon name="cart"></ion-icon></a>
+            </div>
+          </div>
+
+          <div class="padding  col-6 col-md-4 col-lg-3">
+            <div class="producto">
+                <a href="/"><img src="/img/a1.jpg" style="" class="img-productos"></a>
+                <h3>Zapatilla</h3>
+                <p class="descripcion">Yeezy</p>
+              <a class="ordenar" href="/">Ordenar!  <ion-icon name="cart"></ion-icon></a>
+            </div>
+          </div>
+
+        </div>
+      </section>
+    </div>
 
 @endsection
