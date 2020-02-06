302757b7d4831c7d1950977299228b139ef63659
Caminaur <jacaminaur@hotmail.com>
2020-02-06 01:47:05 -0300

    Un poco de control al agregar un producto al carrito

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Product;
+use App\Stock;
+use App\Size;
 class CartController extends Controller
 {
 
 
 
-  public function addToCart(){
-    
+  public function addToCart(Request $request){
+    $product = Product::find($request->id);
+    $size = Size::where('name','=',$request->size)->get();
+    $cantidad_de_stock = Stock::where('product_id','=',$request->id)->where('size_id', '=', $size[0]->id)->get()[0]->quantity;
+    if ($request->quantity>$cantidad_de_stock) {
+      return back();
+    }
+    else {
+      return view('/cart');
+    }
   }
 }
